60ee47641dd247583ef1c100838ef69b0fcd4c65
asiedusamuel <mybusinessconcepts@gmail.com>
2019-10-03 13:04:39 +0100

    index page added

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="container">
+    <img data-src="~assets/images/logo.png"  class="lazyload" />
+    <h1>Chatty Web</h1>
+</div>
+</template>
+<style lang="scss">
+    .container{
+        margin: 10% auto;
+        width: 600px;
+        text-align:center;
+        font-family: Arial, Helvetica, sans-serif;
+        color: rgb(144, 250, 129);
+        font-weight: 300;
+    }
+</style>
